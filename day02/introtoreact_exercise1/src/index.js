4a71a8dbef52a923001045935183525d645b4cff
Sarah <vanwars@gmail.com>
2021-01-05 12:30:25 -0600

    Finished exercise 1

--- day02/introtoreact_exercise1/src/index.js
+++ day02/introtoreact_exercise1/src/index.js
@@ -1,11 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+// JavaScript logic:
+const firstName = 'Sarah';
+const currentYear = new Date().getFullYear();
+const getHeaderColor = () => {
+    const hour = new Date().getHours();
+    console.log(hour);
+    if (hour <= 11) {
+        return 'red'; // 12AM - 11:59AM
+    }
+    if (hour < 18) {
+        return 'green'; // 12PM - 5:59PM
+    }
+    return 'blue'; // on or after 6PM
+};
+const headerStyles = {color: getHeaderColor()};
+
+
+// Adding tags + values to the DOM:
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <div>
-    <h1 className="myHeader">Hello World</h1>
-    <p>This is a paragraph.</p>
+    <h1 style={headerStyles}>Hello World</h1>
+    <p>This is a paragraph. Here is a list:</p>
+    <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+    </ul>
+    <p className="createdBy">Created by {firstName}</p>
+    <p>Copyright {currentYear}</p>
   </div>,
   rootElement
 );
