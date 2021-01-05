4352c62fb7d88b7b57133552760613fd1413f2b0
Sarah <vanwars@gmail.com>
2021-01-05 13:54:02 -0600

    built both react apps for hosting in GitHub

---
+++ day02/introtoreact_exercise1/build/precache-manifest.8a37058f76d3039ff65ec6ae6939a4da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6833eed0bcb24537b84771608eaf753a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "19a1fcc655ecd1ae747b",
+    "url": "./static/js/2.86d61809.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "./static/js/2.86d61809.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be541d7a63900f214fab",
+    "url": "./static/js/main.eb8d7968.chunk.js"
+  },
+  {
+    "revision": "cdd5ce40c7dc7a80e4c6",
+    "url": "./static/js/runtime-main.f1218f2f.js"
+  }
+]);
