d7fb1ae4e585f27c91bda200188384dc0bb76a7f
Sarah <vanwars@gmail.com>
2021-01-05 13:24:44 -0600

    deployed

---
+++ day02/card-component/build/precache-manifest.58b70db78659fe350cbec21014b33cfc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8edd989f3d54e83fd955d536819c3e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9774223031413cce4a6",
+    "url": "/static/js/2.bb1c81ea.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/static/js/2.bb1c81ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0163b806f17bc2116fa",
+    "url": "/static/js/main.0096e37a.chunk.js"
+  },
+  {
+    "revision": "b5d083244549c0fc6ced",
+    "url": "/static/js/runtime-main.cc0c76af.js"
+  }
+]);
---
+++ day02/card-component/src/index.js
@@ -0,0 +1,55 @@
+import React from "react";
+import ReactDOM from "react-dom";
+//import App from "./components/App";
+
+//ReactDOM.render(<App />, document.getElementById("root"));
+
+function Card(props) {
+  return (
+    <div className="card-holder">
+        <div class="card">
+            <h2>{props.name}</h2>
+            <img src={props.src} alt="avatar_img" class="img1" />
+            <p>{props.phone}</p>
+            <p>{props.email}</p>
+        </div>
+    </div>
+  );
+}
+
+//Similar to html objects such as input where we have inputs.
+
+ReactDOM.render(
+    <div>
+        <h1>My Contacts</h1>
+        <div className="card-container">
+            <Card
+                name="Dog"
+                src="/images/alexandru-rotariu-o_QTeyGVWjQ-unsplash.jpg"
+                phone="+123 456 789"
+                email="s@g.com"
+            />
+            <Card
+                name="Jack Cat"
+                src="/images/marko-blazevic-zBvVuRJ71vU-unsplash.jpg"
+                phone="+987 654 321"
+                email="jack@nowhere.com"
+            />
+            <Card
+                name="Chuck Panda"
+                src="/images/sid-balachandran-_9a-3NO5KJE-unsplash.jpg"
+                phone="+918 372 574"
+                email="gmail@chuck.com"
+            />
+            <Card
+                name="Chuck Panda"
+                src="/images/sid-balachandran-_9a-3NO5KJE-unsplash.jpg"
+                phone="+918 372 574"
+                email="gmail@chuck.com"
+            />
+        </div>
+    </div>,
+  document.getElementById("root")
+);
+
+//Show how to access the values using vanilla Javascript.
