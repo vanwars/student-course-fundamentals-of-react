030601936f1e3de2ed262d12f7c979b52eab766b
Sarah <vanwars@gmail.com>
2021-01-05 12:19:30 -0600

    adding exercise from today

---
+++ day02/introtoreact_exercise1/src/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <div>
+    <h1 className="myHeader">Hello World</h1>
+    <p>This is a paragraph.</p>
+  </div>,
+  rootElement
+);
+
+//Create a react app from scratch
+//Have it display a Paragraph with a heading and a body
+//Display a list of ANYTHING.
+//Created by YOURNAME.
+//Copyright CURRENTYEAR. (Bonus points for making a dynamic Current year)
+
+//Style your classes
+
+//Always use console.log("") to log variables.
+
+//use the style.css to make the fontj size of the "Created by YOURNAME." to be 30px
+
+//extra credit
+// Dynamically change the color of the h1 using inline css styles.
+
+//ie Morning = red, Afternoon= green, night = blue
+
+//hint = const date = new Date():
+//const currentTime = date.getHours():
+
+//To access a Javascript object we do ObjectName.Attribute
---
+++ hw02/src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import notes from "../notes";
+
+function App() {
+  return (
+    <div>
+        <h1>Hello world!</h1>
+        <p>This is a test!</p>
+    </div>
+  );
+}
+
+export default App;
